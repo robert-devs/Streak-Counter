fdf072f8d18587e70b974d6073e6e7231bf04b7b
robert-devs <mainrobert04@gmail.com>
2022-08-03 15:49:24 +0300

    upadates

--- dist/main.js
+++ dist/main.js
@@ -2,7 +2,7 @@ const streakContainer = document.getElementById("mytask");
 const taskName = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const imageInput = document.getElementById("image-input");
-const addTask = document.getElementById("add-task");
+const addTaskBtn = document.getElementById("add-task");
 const addBtnIcon = document.querySelector(".icon-inner svg");
 const showAddForm = () => {
     document.querySelector(".main").style.display = "none";
@@ -12,20 +12,28 @@ const hideAddForm = () => {
     document.querySelector(".main").style.display = "flex";
     document.querySelector(".body").style.display = "none";
 };
+setTimeout(() => { }, 1000);
 class tasks {
     constructor() {
         this.taskArray = [];
         this.loadTasks = () => {
             if (this.taskArray.length > 0) {
                 const taskHtml = this.taskArray.map(({ id, date, taskName, imageInput }) => {
-                    const difference = (new Date() - new Date(date)) / 1000 / 60 / 60 / 24;
-                    let show = difference < 0 ? " Done In Time" : `${Math.floor(difference)} Days`;
+                    const difference = (new Date() - new Date(date)) /
+                        1000 /
+                        60 /
+                        60 /
+                        24;
+                    let show = difference < `${Math.floor(difference)} Days`;
                     const element = `<li class="task-item   id="${id}" >
               
-                <div class="task-body" >
-                    <p> ${imageInput}data-id="${id}</p>
-                    <h4>${date} <span class="due-date"></span></h4>
-                    <p>${taskName}<span class="name">${taskName} data-id="${id}</span></p>
+                    <div class="task-body" >
+                         <img src="${imageInput} "style= width:250px ,height:300px;alt="">
+                           <h3>${taskName}</>
+                            <h6>${date} </h6>
+                            
+                    </div>
+                   
                     
                 </li>`;
                     return element;
@@ -33,7 +41,8 @@ class tasks {
                 streakContainer.innerHTML = taskHtml.join("");
             }
             else {
-                streakContainer.innerHTML = "<h2 style='text-align: center;'>No Streak added Yet</h2>";
+                streakContainer.innerHTML =
+                    "<h2 style='text-align: center;'>No Streak added Yet</h2>";
             }
         };
         this.addEventListener = () => {
@@ -60,23 +69,23 @@ const newTodosubmits = (e) => {
         return;
     }
     if (!dateValue) {
-        alert("taskName is required");
+        alert("dateValue is required");
         return;
     }
     if (!imageValue) {
-        alert("taskName is required");
+        alert("imageValue is required");
         return;
     }
     const newTodo = {
         taskName: taskNamevalue,
         date: dateValue,
         imageInput: imageValue,
-        id: Math.floor(Math.random() * 10000000)
+        id: Math.floor(Math.random() * 10000000),
     };
     todos.addTask(newTodo);
     taskName.value = "";
     dateInput.value = "";
     imageInput.value = "";
 };
-// export {};
+export {};
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,105 +1,105 @@
-import {Task} from "./interfaces/task.interface.js";
+import { Task } from "./interfaces/task.interface.js";
 
-const streakContainer = document.getElementById("mytask")
-const taskName = document.getElementById("task-input") as HTMLInputElement
-const dateInput = document.getElementById("date-input") as HTMLInputElement
-const imageInput = document.getElementById("image-input") as HTMLInputElement
-const addTask = document.getElementById("add-task") as HTMLButtonElement
+const streakContainer = document.getElementById("mytask");
+const taskName = document.getElementById("task-input") as HTMLInputElement;
+const dateInput = document.getElementById("date-input") as HTMLInputElement;
+const imageInput = document.getElementById("image-input") as HTMLInputElement;
+const addTaskBtn = document.getElementById("add-task") as HTMLButtonElement;
 const addBtnIcon = document.querySelector(".icon-inner svg") as HTMLElement;
 
+const showAddForm = () => {
+  (document.querySelector(".main") as HTMLElement).style.display = "none";
+  (document.querySelector(".body") as HTMLElement).style.display = "block";
+};
+const hideAddForm = () => {
+  (document.querySelector(".main") as HTMLElement).style.display = "flex";
+  (document.querySelector(".body") as HTMLElement).style.display = "none";
+};
 
-const showAddForm = ()=>{
-    (document.querySelector(".main") as HTMLElement).style.display = "none";
-    (document.querySelector(".body") as HTMLElement).style.display = "block"
-}
-const hideAddForm = ()=>{
-    (document.querySelector(".main") as HTMLElement).style.display = "flex";
-    (document.querySelector(".body") as HTMLElement).style.display = "none"
-}
-
+setTimeout(() => {}, 1000);
 
 class tasks {
-    private taskArray: Task[] = []
-    constructor() {
-        this.addEventListener()
-        this.loadTasks()
-    }
-    addTask(task: Task) {
-        this.taskArray.push(task)
-        this.loadTasks()
-    }
-    getTask() {
-        return this.taskArray
-    }
+  private taskArray: Task[] = [];
+  constructor() {
+    this.addEventListener();
+    this.loadTasks();
+  }
+  addTask(task: Task) {
+    this.taskArray.push(task);
+    this.loadTasks();
+  }
+  getTask() {
+    return this.taskArray;
+  }
 
-    loadTasks = () => {
-        if (this.taskArray.length > 0) {
-            const taskHtml = this.taskArray.map(({ id, date, taskName, imageInput }) => {
-                const difference: any = ((new Date() as any) - (new Date(date) as any)) / 1000 / 60 / 60 / 24
+  loadTasks = () => {
+    if (this.taskArray.length > 0) {
+      const taskHtml = this.taskArray.map(
+        ({ id, date, taskName, imageInput }) => {
+          const difference: any =
+            ((new Date() as any) - (new Date(date) as any)) /
+            1000 /
+            60 /
+            60 /
+            24;
 
-                let show = difference < 0 ? " Done In Time" : `${Math.floor(difference)} Days`
+          let show = difference < `${Math.floor(difference)} Days`;
 
-
-
-                const element: string = `<li class="task-item   id="${id}" >
+          const element: string = `<li class="task-item   id="${id}" >
               
-                <div class="task-body" >
-                    <p> ${imageInput}data-id="${id}</p>
-                    <h4>${date} <span class="due-date"></span></h4>
-                    <p>${taskName}<span class="name">${taskName } data-id="${id}</span></p>
+                    <div class="task-body" >
+                         <img src="${imageInput} "style= width:250px ,height:300px;alt="">
+                           <h3>${taskName}</>
+                            <h6>${date} </h6>
+                            
+                    </div>
+                   
                     
-                </li>`
-                return element
-            })
-            streakContainer!.innerHTML = taskHtml.join("")
-
-            // this.addEventListeners()
-        } else {
-            streakContainer!.innerHTML = "<h2 style='text-align: center;'>No Streak added Yet</h2>"
+                </li>`;
+          return element;
         }
+      );
+      streakContainer!.innerHTML = taskHtml.join("");
+
+      // this.addEventListeners()
+    } else {
+      streakContainer!.innerHTML =
+        "<h2 style='text-align: center;'>No Streak added Yet</h2>";
     }
-    addEventListener =() =>{
-        const addTaskBtn = document.getElementById("add-task") as HTMLButtonElement;
+  };
+  addEventListener = () => {
+    const addTaskBtn = document.getElementById("add-task") as HTMLButtonElement;
 
-        // console.log("fgfgfgfg",addTaskBtn);
-        
-        
-
-    }
-
-
-
-
-}
- 
-const todos = new tasks()
-const newTodosubmits =(e:any)=>{
-    const taskNamevalue = taskName.value
-    const dateValue = dateInput.value
-    const imageValue = imageInput.value
-
-    if(!taskNamevalue){
-        alert("taskName is required")
-        return
-    }
-     if(!dateValue){
-        alert("taskName is required")
-        return
-    }
-     if(!imageValue){
-        alert("taskName is required")
-        return
-    } 
-     const newTodo:Task = {
-        taskName:taskNamevalue,
-        date: dateValue,
-        imageInput:imageValue,
-        id: Math.floor(Math.random() * 10000000)
-    };
-    todos.addTask(newTodo);
-    taskName.value = "";
-    dateInput.value = "";
-    imageInput.value = ""
+    // console.log("fgfgfgfg",addTaskBtn);
+  };
 }
 
+const todos = new tasks();
+const newTodosubmits = (e: any) => {
+  const taskNamevalue = taskName.value;
+  const dateValue = dateInput.value;
+  const imageValue = imageInput.value;
 
+  if (!taskNamevalue) {
+    alert("taskName is required");
+    return;
+  }
+  if (!dateValue) {
+    alert("dateValue is required");
+    return;
+  }
+  if (!imageValue) {
+    alert("imageValue is required");
+    return;
+  }
+  const newTodo: Task = {
+    taskName: taskNamevalue,
+    date: dateValue,
+    imageInput: imageValue,
+    id: Math.floor(Math.random() * 10000000),
+  };
+  todos.addTask(newTodo);
+  taskName.value = "";
+  dateInput.value = "";
+  imageInput.value = "";
+};
